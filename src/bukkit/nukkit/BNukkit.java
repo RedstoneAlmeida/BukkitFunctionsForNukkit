3a7d686c0dd3c8b227b4c9b6aa5c87fa5aaaff19
RedstoneAlmeida <mineps3marcos@gmail.com>
2017-01-14 23:53:00 -0200

    Update BNukkit.java

--- src/bukkit/nukkit/BNukkit.java
+++ src/bukkit/nukkit/BNukkit.java
@@ -4,5 +4,15 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 
 public class BNukkit extends PluginBase implements Listener{
+  
+  public static void createInventory(Player player, int size){
+        createInventory(player, size, null);
+  }
+
+  public static void createInventory(Player player, int size, String name){
+       if(name == null){
+          return;
+       }
+  }
 
 }
